3a56a79484435c05bc7c222192fb7413c1153d31
StrawberryDan <DanRhysMorris@gmail.com>
2023-11-04 17:29:27 +0000

    Add hierarchical positioning, sizing and scaling to Node

--- src/Node.cpp
+++ src/Node.cpp
@@ -1,1 +1,56 @@
 #include "Node.hpp"
+
+
+namespace Strawberry::UI
+{
+	Node::Node()
+		: mParent(nullptr)
+	{}
+
+
+	Node::Node(Node& node)
+		: mParent(node)
+	{}
+
+
+	Core::Math::Vec2i Node::GetPosition() const
+	{
+		return mParent ? mParent->GetPosition() : Core::Math::Vec2i() + GetLocalPosition();
+	}
+
+
+	Core::Math::Vec2i Node::GetLocalPosition() const
+	{
+		return mLocalPosition;
+	}
+
+
+	Core::Math::Vec2i Node::GetSize() const
+	{
+		return mParent ? mParent->GetSize() : Core::Math::Vec2i() + GetLocalSize();
+	}
+
+
+	Core::Math::Vec2i Node::GetLocalSize() const
+	{
+		return mLocalSize;
+	}
+
+
+	Core::Math::Vec2f Node::GetScale() const
+	{
+		return mParent ? mParent->GetScale() : Core::Math::Vec2f() + GetLocalScale();
+	}
+
+
+	Core::Math::Vec2f Node::GetLocalScale() const
+	{
+		return mLocalScale;
+	}
+
+
+	Core::ReflexivePointer<Node> Node::GetChild(size_t index)
+	{
+		return mChildren[index]->GetReflexivePointer();
+	}
+}
--- src/Node.hpp
+++ src/Node.hpp
@@ -1,9 +1,43 @@
 #pragma once
 
 
+// Strawberry Core
+#include <Strawberry/Core/Types/ReflexivePointer.hpp>
+#include <Strawberry/Core/Math/Vector.hpp>
+// Standard Library
+#include <memory>
+#include <vector>
+
+
 namespace Strawberry::UI
 {
 	class Node
+		: public Core::EnableReflexivePointer<Node>
 	{
+	public:
+		Node();
+		Node(Node& node);
+
+
+		Core::Math::Vec2i GetPosition() const;
+		Core::Math::Vec2i GetLocalPosition() const;
+
+		Core::Math::Vec2i GetSize() const;
+		Core::Math::Vec2i GetLocalSize() const;
+
+		Core::Math::Vec2f GetScale() const;
+		Core::Math::Vec2f GetLocalScale() const;
+
+
+		Core::ReflexivePointer<Node> GetChild(size_t index);
+
+
+	private:
+		Core::ReflexivePointer<Node> mParent;
+		std::vector<std::unique_ptr<Node>> mChildren;
+
+		Core::Math::Vec2i mLocalPosition;
+		Core::Math::Vec2i mLocalSize;
+		Core::Math::Vec2f mLocalScale;
 	};
 }
