df0291dd1c1d43cc17dc9f629e3ff33817280831
StrawberryDan <DanRhysMorris@gmail.com>
2023-12-22 15:57:47 +0000

    Add fmt::format overloads to UI::Text

--- src/Strawberry/UI/Text.hpp
+++ src/Strawberry/UI/Text.hpp
@@ -7,6 +7,8 @@
 #include "RectangularNode.hpp"
 // Strawberry Graphics
 #include "Strawberry/Graphics/2D/FontFace.hpp"
+// Lib Format
+#include <fmt/format.h>
 // Standard Library
 #include <string>
 #include <memory>
@@ -25,6 +27,12 @@ namespace Strawberry::UI
 		Text(const Graphics::FontFace& fontFace, unsigned int fontSize);
 		Text(const Graphics::FontFace& fontFace, unsigned int fontSize, const std::string& string);
 		Text(const Graphics::FontFace& fontFace, unsigned int fontSize, const std::u32string& string);
+
+
+		template<typename ...Args>
+		constexpr Text(const Graphics::FontFace& fontFace, unsigned int fontSize, const std::string& string, Args... args)
+			: Text(fontFace, fontSize, fmt::format(fmt::runtime(string), args...))
+		{}
 
 
 		void Render(Renderer& renderer) override;
@@ -46,6 +54,13 @@ namespace Strawberry::UI
 		void SetString(const std::u32string& string);
 		void SetString(const std::u8string& string);
 		void SetString(const std::string& string);
+
+
+		template<typename ...Args>
+		void SetString(const std::string& string, Args... args)
+		{
+			SetString(fmt::format(fmt::runtime(string), args...));
+		}
 
 
 	protected:
